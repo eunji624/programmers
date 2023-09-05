26a657509182a467a036a0a827fba9d6f3cd5acd
010D <130081021+eunji624@users.noreply.github.com>
2023-09-05 14:21:58 +0900

    [level 0] Title: 각도기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.js
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -1,13 +1,3 @@
 function solution(angle) {
-    
-    if(angle < 90){
-       return 1;
-    } else if(angle === 90){
-       return 2;
-    } else if(angle < 180){
-       return 3;
-    } else {
-       return 4;
-    }
-  
+    return [0,90,91,180].filter((e)=> angle >= e).length;
 }
