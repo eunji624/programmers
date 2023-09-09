3aa1bb4d0e043ce9441ba72904c3edfd0d2b14e9
010D <130081021+eunji624@users.noreply.github.com>
2023-09-10 03:13:06 +0900

    [unrated] Title: 날짜 비교하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
+++ 프로그래머스/unrated/181838. 날짜 비교하기/날짜 비교하기.js
@@ -1,15 +1,8 @@
 function solution(date1, date2) {
-    // let answer;
-    // date1.forEach((e)=> {
-    //     date2.forEach((i)=>{
-    //         e<i?  answer= 1 : answer= 0
-    //     })
-    // })
-    
-        let one = Number(date1.toString().replaceAll(',', ''))
-        let two = Number(date2.toString().replaceAll(',', ''))
-        if((one - two) < 0){
-            return 1
-        }else {return 0}
- 
+    let one = Number(date1.toString().replaceAll(',', ''))
+    let two = Number(date2.toString().replaceAll(',', ''))
+    if((one - two) < 0){
+        return 1
+    }else {return 0}
+
 }
