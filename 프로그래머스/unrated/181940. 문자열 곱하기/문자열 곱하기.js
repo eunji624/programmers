e368edbacafa141be8299bfa93ee871779fd047b
010D <130081021+eunji624@users.noreply.github.com>
2023-09-10 02:31:57 +0900

    [unrated] Title: 문자열 곱하기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181940. 문자열 곱하기/문자열 곱하기.js
@@ -0,0 +1,9 @@
+function solution(my_string, k) {
+    // var answer ="";
+    // for(let i=1; i< k+1; i++){
+    //     answer += my_string
+    // }
+    // return answer;
+    
+    return my_string.repeat(k)
+}
