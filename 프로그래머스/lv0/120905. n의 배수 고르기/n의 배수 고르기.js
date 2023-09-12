ccecd6926411c8148e1a688c5ffca18bbbc38aa5
010D <130081021+eunji624@users.noreply.github.com>
2023-09-12 23:53:33 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -0,0 +1,7 @@
+function solution(n, numlist) {
+    return numlist.filter((e)=> e%n===0)
+    // let answer = []
+    // numlist.forEach(
+    //     (e)=> e%n ===0 ? answer.push(e): answer)
+    // return answer 
+}
