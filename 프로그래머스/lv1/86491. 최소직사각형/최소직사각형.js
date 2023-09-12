494ee78f16316263d4c2203321a44c819da2764a
010D <130081021+eunji624@users.noreply.github.com>
2023-09-13 00:47:11 +0900

    [level 1] Title: 최소직사각형, Time: 6.89 ms, Memory: 42.1 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.js
@@ -1,17 +1,28 @@
 function solution(sizes) {
-    function bigsmall(arr){
-        arr.sort((a,b)=> {return b-a});
-    }
-    let frist = [];
-        second = []
-    sizes.map((a)=>{
-        bigsmall(a)
-        frist.push(a[0]);
-        second.push(a[1]);
+    let A = [];
+    let B = [];
+    sizes.map(([a,b]) => {
+        let near = [a,b].sort((a,b)=> {return a-b});
+        A.push(near[0]); 
+        B.push(near[1]);
     })
-    bigsmall(frist);
-    bigsmall(second);
+    return  Math.max(...A) * Math.max(...B)
+    
+    
+//     function bigsmall(arr){
+//         arr.sort((a,b)=> {return b-a});
+//     }
+//     let frist = [];
+//         second = []
+//     sizes.map((a)=>{
+//         bigsmall(a)
+//         frist.push(a[0]);
+//         second.push(a[1]);
+//     })
+//     bigsmall(frist);
+//     bigsmall(second);
 
-    var answer = frist[0] * second[0];
-    return answer;
+//     var answer = frist[0] * second[0];
+//     return answer;
+    
 }
