efa022a98d8548d2371b67bf0dc08854ecf30dab
010D <130081021+eunji624@users.noreply.github.com>
2023-09-13 22:59:38 +0900

    [unrated] Title: 접미사인지 확인하기, Time: 0.44 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -0,0 +1,7 @@
+function solution(my_string, is_suffix) {
+    let n = [];
+    for(let i=0; i<my_string.length; i++){
+        n.push([...my_string].slice(i).join(''));
+    }
+    return  n.includes(is_suffix)? 1 : 0;
+}
