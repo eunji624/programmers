32f6c657dfba790bba9dc8d2c167176ada4a1bfd
010D <130081021+eunji624@users.noreply.github.com>
2023-09-21 00:53:26 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.23 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -3,11 +3,11 @@ function solution(array) {
     for(let i=0; i<array.length; i++){
         arr.set(array[i], (arr.get(array[i]) || 0)+1); 
     }
-    let max = Math.max(...[...arr].map((a)=>{return a[1]}))
-    let count = [...arr].filter((a)=>{return a[1] ===max}).length
+    let max = Math.max(...[...arr].map(a=>a[1]))
+    let count = [...arr].filter(a=> a[1] ===max).length
     if(count >1){
         return -1 
     }else{
-        return count > 1 ? -1 : [...arr].find((v) => {return v[1] === max})[0];
+        return  [...arr].find(v=> v[1] === max)[0];
     };
 }
