ac9346edf62d38e876afeaa798cca7bb57ef8aab
010D <130081021+eunji624@users.noreply.github.com>
2023-09-20 19:53:03 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,36 +1,13 @@
 function solution(array) {
     let arr = new Map();
-    for(let i = 0; i < array.length; i++){
-      arr.set(array[i], (arr.get(array[i])||0) + 1);
+    for(let i=0; i<array.length; i++){
+        arr.set(array[i], (arr.get(array[i]) || 0)+1); 
     }
-    let max = Math.max(...[...arr].map(v => v[1]))
-    let count = [...arr].filter(v => v[1] === max).length
-    return count > 1 ? -1 : [...arr].find(v => v[1] === max)[0]
-
-    // let answer;
-    // let obj = {};
-    // for(let i=0; i<array.length; i++){
-    //     let v=array[i];
-    //     if(i>=1 && obj[v]){
-    //         obj[v]++;
-    //     }else{
-    //         obj[v] = 1
-    //     }
-    // }
-    // let val = Object.values(obj);
-    // let key = Object.keys(obj);
-    // let maxVal = Math.max(...val);
-    // let minVal = Math.min(...val);
-    // let index = val.indexOf(maxVal);
-    // let slice = val.slice(index+1, index-1);
-    // let remaxVal = slice.includes(maxVal)
-    // if(val.length>1 && remaxVal){
-    //     answer = -1;
-    // }else{
-    //     answer = key[index]
-    // }
-    // return parseInt(answer)
-    // // return val
-    // // return index
-    // // return remaxVal
+    let max = Math.max(...[...arr].map((a)=>{return a[1]}))
+    let count = [...arr].filter((a)=>{return a[1] ===max}).length
+    if(count >1){
+        return -1 
+    }else{
+        return count > 1 ? -1 : [...arr].find((v) => {return v[1] === max})[0];
+    };
 }
