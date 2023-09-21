5d74e80821ee3b267ee5598d9ae41e6f91ae8a86
010D <130081021+eunji624@users.noreply.github.com>
2023-09-21 12:41:17 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.js
@@ -1,13 +1,31 @@
 function solution(array) {
-    let arr = new Map();
-    for(let i=0; i<array.length; i++){
-        arr.set(array[i], (arr.get(array[i]) || 0)+1); 
+    // let arr = new Map();
+    // for(let i=0; i<array.length; i++){
+    //     arr.set(array[i], (arr.get(array[i]) || 0)+1); 
+    // }
+    // let max = Math.max(...[...arr].map(a=>a[1]))
+    // let count = [...arr].filter(a=> a[1] ===max).length
+    // if(count >1){
+    //     return -1 
+    // }else{
+    //     return  [...arr].find(v=> v[1] === max)[0];
+    // };
+
+
+    const obj = {};
+    let val = 0;
+    let key = 0;
+    
+    for(let e of array){
+        obj[e]===undefined? obj[e] = 1 : obj[e]++
+        
+        if(obj[e]>val){
+            val = obj[e];
+            key = e;
+        }else if(obj[e] === val && e!==key ){
+            key = -1;
+        }
     }
-    let max = Math.max(...[...arr].map(a=>a[1]))
-    let count = [...arr].filter(a=> a[1] ===max).length
-    if(count >1){
-        return -1 
-    }else{
-        return  [...arr].find(v=> v[1] === max)[0];
-    };
+    return key;
+
 }
