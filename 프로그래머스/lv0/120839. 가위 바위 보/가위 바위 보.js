baa357674dcfef5324c79a90e2eadea73b54db48
010D <130081021+eunji624@users.noreply.github.com>
2023-09-21 17:04:28 +0900

    [level 0] Title: 가위 바위 보, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,19 +1,21 @@
 function solution(rsp){
-    // let arr=[...rsp];
-    // let a = new Array();
-    // arr.forEach((e)=>{
-    //     e = Number(e)
-    //     if(e===2){a.push(0)}
-    //     else if( e===0){a.push(5)}
-    //     else if (e === 5){a.push(2)}
-    // })
-    // return a.join('')
+    let a = '';
+    [...rsp].map((e)=>{
+        a+= e==='2' ?e ='0' : e==='0' ? e='5' : e='2';
+    }).join('')
+    return a
     
-    let arr = {
-        2 : 0,
-        0 : 5,
-        5 : 2
-    };
-    return  [...rsp].map((e)=> arr[e]).join('')
+    // let a = '';
+    // [...rsp].map((e)=>{
+    //     e === '2' ? a += '0' : e === '0' ? a += '5' : a += '2';
+    // });
+    // return a;
+
+    // let arr = {
+    //     2 : 0,
+    //     0 : 5,
+    //     5 : 2
+    // };
+    // return  [...rsp].map((e)=> arr[e]).join('')
 
 }
