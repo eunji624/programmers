799ee4278822c51ef96c586f8239c01edc8928ba
010D <130081021+eunji624@users.noreply.github.com>
2023-09-21 16:30:37 +0900

    [level 0] Title: 가위 바위 보, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120839. 가위 바위 보/README.md
+++ 프로그래머스/lv0/120839. 가위 바위 보/README.md
@@ -1,10 +1,10 @@
 # [level 0] 가위 바위 보 - 120839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120839) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120839?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.25 ms
+메모리: 33.5 MB, 시간: 0.08 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
+++ 프로그래머스/lv0/120839. 가위 바위 보/가위 바위 보.js
@@ -1,12 +1,19 @@
 function solution(rsp){
-    let arr=[...rsp];
-    let a = new Array();
-    arr.forEach((e)=>{
-        e = Number(e)
-        if(e===2){a.push(0)}
-        else if( e===0){a.push(5)}
-        else if (e === 5){a.push(2)}
-    })
-    return a.join('')
+    // let arr=[...rsp];
+    // let a = new Array();
+    // arr.forEach((e)=>{
+    //     e = Number(e)
+    //     if(e===2){a.push(0)}
+    //     else if( e===0){a.push(5)}
+    //     else if (e === 5){a.push(2)}
+    // })
+    // return a.join('')
+    
+    let arr = {
+        2 : 0,
+        0 : 5,
+        5 : 2
+    };
+    return  [...rsp].map((e)=> arr[e]).join('')
 
 }
