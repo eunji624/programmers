b045abec88e8ff309143466359ad108561a6b72c
010D <130081021+eunji624@users.noreply.github.com>
2023-09-26 14:17:34 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.js
@@ -1,13 +1,16 @@
 function solution(my_string) {
-    var answer = [];
-    let arr =  [...my_string]
-    arr.forEach((e, i)=> {
-        arr[i] = Number(e)
-        if(e>= 0){answer.push(parseInt(e))}
-    })
-    answer.sort((a,b)=>{
-        return a-b
-    })
-    return answer
+    // var answer = [];
+    // let arr =  [...my_string]
+    // arr.forEach((e, i)=> {
+    //     arr[i] = Number(e)
+    //     if(e>= 0){answer.push(parseInt(e))}
+    // })
+    // answer.sort((a,b)=>{
+    //     return a-b
+    // })
+    // return answer
     
+    let arr = Array.from(my_string).filter((a)=> a>=0);
+    return arr.map((a)=>parseInt(a)).sort((a,b)=> a-b);
+
 }
