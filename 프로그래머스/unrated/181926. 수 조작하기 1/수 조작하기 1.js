ee69a36a11417e34216ca8ab44179ac6007a8e83
010D <130081021+eunji624@users.noreply.github.com>
2023-10-13 10:50:13 +0900

    [level unrated] Title: 수 조작하기 1, Time: 1.95 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
+++ 프로그래머스/unrated/181926. 수 조작하기 1/수 조작하기 1.js
@@ -1,27 +1,37 @@
-function solution(n, control) {
-    // let arr = control.split('');
-    // arr.forEach((e)=>{
-    //     switch(e){
-    //         case "w":
-    //             n+=1;
-    //             break;
-    //         case "s" :
-    //             n-=1;
-    //             break;
-    //         case "d" :
-    //             n+= 10;
-    //             break;
-    //         case "a" :
-    //             n-= 10;
-    //     }
-    // })
-    
-    let obj = {
-        w : +1,
-        s : -1,
-        d : +10,
-        a : -10
-    }
-    control.split('').map(e=> n+=obj[e])
-    return n;
+let obj = {
+    w : (n)=> n+1,
+    s : (n)=> n-1,
+    d : (n)=> n+10,
+    a : (n)=> n-10,
 }
+function solution(n, control){
+    return [...control].reduce((acc, cur)=> obj[cur](acc),n)
+}
+
+// function solution(n, control) {
+//     // let arr = control.split('');
+//     // arr.forEach((e)=>{
+//     //     switch(e){
+//     //         case "w":
+//     //             n+=1;
+//     //             break;
+//     //         case "s" :
+//     //             n-=1;
+//     //             break;
+//     //         case "d" :
+//     //             n+= 10;
+//     //             break;
+//     //         case "a" :
+//     //             n-= 10;
+//     //     }
+//     // })
+    
+//     let obj = {
+//         w : +1,
+//         s : -1,
+//         d : +10,
+//         a : -10
+//     }
+//     control.split('').map(e=> n+=obj[e])
+//     return n;
+// }
