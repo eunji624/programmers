514332be7ab8c5b22526be79978d7f46a0ecf97a
010D <130081021+eunji624@users.noreply.github.com>
2023-10-20 09:28:52 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -2,13 +2,7 @@ function solution(n) {
 //     var answer = '수박';
 //     let x = Math.ceil(n/2);
 //     let str = answer.repeat(x)
-   
 //     return x*2 > n ? str.substr(0, n) : str
-    
-    
-    let str = '수박'.repeat(n/2+1)
-    return str.length > n ? str.substr(0, n) : str;
-    
-    
-    
+
+    return '수박'.repeat(n/2+1).substr(0, n);
 }
