f6d47cbad4b6696e1253c9444f6e414af15af9f3
010D <130081021+eunji624@users.noreply.github.com>
2023-10-23 11:21:32 +0900

    [level 1] Title: 폰켓몬, Time: 0.23 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,9 +1,12 @@
 function solution(nums) {
-    let way = [];
-    nums.map((e)=>{
-        if(!way.includes(e)){
-            way.push(e)
-        }
-    })
-    return  Math.min(nums.length/2, way.length);
+    let setNums = new Set(nums);
+    return Math.min(setNums.size, nums.length/2)
+    
+    // let way = [];
+    // nums.map((e)=>{
+    //     if(!way.includes(e)){
+    //         way.push(e)
+    //     }
+    // })
+    // return  Math.min(nums.length/2, way.length);
 }
