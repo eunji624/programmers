94709506e03035ce085baa4185db314d58ffe47b
010D <130081021+eunji624@users.noreply.github.com>
2023-11-21 09:44:02 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 3.22 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,5 +1,6 @@
 function solution(phone) {
     let show = [...phone].splice(-4);
+    console.log(show)
     let star = [...phone].splice(0, phone.length-4)
     return [...star.map((e)=> e="*"), ...show].join("")
 }
