02bac5a1c1b16efe885c4e621ccdf9f15d7babeb
010D <130081021+eunji624@users.noreply.github.com>
2023-11-22 09:38:07 +0900

    [level 1] Title: 내적, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,11 +1,18 @@
-function solution(a, b) {
-    let newArr = [];
-    a.forEach((ae, ai)=>{
-        b.forEach((be, bi)=> {
-            if(ai=== bi){
-               newArr.push(ae*be)
-            }
-        })
-    })
-    return newArr.reduce((acc,cur)=>acc+cur,0)
+// function solution(a, b) {
+//     let newArr = [];
+//     a.forEach((ae, ai)=>{
+//         b.forEach((be, bi)=> {
+//             if(ai=== bi){
+//                newArr.push(ae*be)
+//             }
+//         })
+//     })
+//     return newArr.reduce((acc,cur)=>acc+cur,0)
+// }
+
+
+function solution(a, b) {    
+    return a.reduce((acc, cur, i)=> {
+        return acc += cur * b[i]
+    }, 0)
 }
