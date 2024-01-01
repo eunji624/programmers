33f92d346031f5225d727b78fade524c064403e5
010D <130081021+eunji624@users.noreply.github.com>
2024-01-02 07:56:11 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 2.38 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
+++ 프로그래머스/1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -1,39 +1,46 @@
-function solution(n, m) {
-    let small = [1, 1, n, m]
-    let big = [n, m]
-    function makeSmall(x){
-        for(let i=2; i<=Math.ceil(x/2); i++){
-            if(x%i ===0) {
-                small.push(i)
-            }else if(x%i !==0){}
-        }
-        return small.sort((a,b)=> b-a);
+// function solution(n, m) {
+function solution(a, b) {
+    var r;
+    for(var ab= a*b;r = a % b;a = b, b = r){
+        console.log(a, b, ab, r)
     }
-    function makeBig(x){
-        let i=2;
-        let a;
-        x===n ? a=m : a=n;
-        while(new Set(big).size  ===  big.length){
-            if((x*i)%a !==0){
-                big.push(x*i);
-                i++; 
-            }else if((x*i)%a ===0){
-                big.push(x*i);
-                i++; 
-                return big
-            }
-        }
-    }
-    makeSmall(n)
-    makeSmall(m)
-    if(n> m){
-        makeBig(m)
-        makeBig(n)
-    }else{
-        makeBig(n)
-        makeBig(m)
-    }
-
-    let finallySmall = small.filter((e,i)=>{return e=== small[i+1]})
-    return [finallySmall[0],big.sort((a,b)=>b-a)[0] ]
+    return [b, ab/b];
 }
+//     let small = [1, 1, n, m]
+//     let big = [n, m]
+//     function makeSmall(x){
+//         for(let i=2; i<=Math.ceil(x/2); i++){
+//             if(x%i ===0) {
+//                 small.push(i)
+//             }else if(x%i !==0){}
+//         }
+//         return small.sort((a,b)=> b-a);
+//     }
+//     function makeBig(x){
+//         let i=2;
+//         let a;
+//         x===n ? a=m : a=n;
+//         while(new Set(big).size  ===  big.length){
+//             if((x*i)%a !==0){
+//                 big.push(x*i);
+//                 i++; 
+//             }else if((x*i)%a ===0){
+//                 big.push(x*i);
+//                 i++; 
+//                 return big
+//             }
+//         }
+//     }
+//     makeSmall(n)
+//     makeSmall(m)
+//     // if(n> m){
+//     //     makeBig(m)
+//     //     makeBig(n)
+//     // }else{
+//         makeBig(n)
+//         makeBig(m)
+//     // }
+
+//     let finallySmall = small.filter((e,i)=>{return e=== small[i+1]})
+//     return [finallySmall[0],big.sort((a,b)=>b-a)[0] ]
+// }
