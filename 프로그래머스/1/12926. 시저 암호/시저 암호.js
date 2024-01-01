a186d34420c7565452ac43bd96caa12522ac0b22
010D <130081021+eunji624@users.noreply.github.com>
2024-01-02 08:30:48 +0900

    [level 1] Title: 시저 암호, Time: 43.98 ms, Memory: 39.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -0,0 +1,18 @@
+function solution(s, n) {
+    let str= "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let abc=  str.toLowerCase()
+    let answer= ''
+    
+    for(let i=0; i<s.length; i++){
+        if(s[i]===' ') {
+            answer+=" ";
+            continue;
+        }
+        const sameTypeArray = str.indexOf(s[i])+1 ? str : abc;
+        let index = sameTypeArray.indexOf(s[i])+n
+        index = (sameTypeArray.length> index) ? index : index-= sameTypeArray.length;
+        console.log('index',index)
+        answer+= sameTypeArray[index]
+    }    
+    return answer
+}
