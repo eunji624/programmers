5938785b7713275cd0fd159cece18ae98c7c71e5
eunji.U <130081021+eunji624@users.noreply.github.com>
2024-07-16 09:16:37 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -1,10 +1,8 @@
 function solution(my_string) {
-    const arr = my_string.split('');
-    const setArr = new Set(arr);
+    const setArr = new Set(my_string);
     let answer='';
     for(let i of setArr.keys()){
         answer += i
-        console.log(answer)
     }
     return answer;
 }
